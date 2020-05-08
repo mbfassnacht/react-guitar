3ba38430687125d9c19259d57082c01215286b3b
Maximo Fassnacht <maximo.fassnacht@exozet.com>
2020-05-08 14:15:07 +0200

    add build

---
+++ docs/precache-manifest.c3251b2e758da0d153e2a10258e68f41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0419982f7741a66fa73e45bdf585de13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06b4b49f1486b8fafa13",
+    "url": "/static/css/main.c2f6eddd.chunk.css"
+  },
+  {
+    "revision": "f5c75ab82db8970a323f",
+    "url": "/static/js/2.fd5a23ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fd5a23ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06b4b49f1486b8fafa13",
+    "url": "/static/js/main.06293ab1.chunk.js"
+  },
+  {
+    "revision": "b610d4fdaa99d4235750",
+    "url": "/static/js/runtime-main.0a5c7bd4.js"
+  }
+]);
